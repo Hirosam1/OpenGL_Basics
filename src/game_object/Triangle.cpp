4152e6937deed5e8be981de06a7b2e8f1b735ae8
Hirosam1 <samuel.pizzatto@outlook.com>
2019-11-05 16:37:41 -0300

    Added a triangle subclass

---
+++ src/game_object/Triangle.cpp
@@ -0,0 +1,14 @@
+#include "game_object/Shape.hpp"
+
+
+Triangle::Triangle() : Shape(24,3){
+    vertex = new float[24]{
+
+     0.0f,   1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,     //Top      
+    -1.0f, -1.0f, 0.0f,    1.0f, 0.0f, 0.0f,    1.0f, 0.0f,    //Left         
+     1.0f,  -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,     //Right
+    };
+    indices = new unsigned int[3]{
+    0,1,2
+    };    
+}
